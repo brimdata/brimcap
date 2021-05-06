8d3154f0c03b3ed1f2f894ec587fa83ca35d0782
Phil Rzewski <phil@brimsecurity.com>
2021-05-06 09:22:21 -0700

    PR feedback: Exec zeek in the Zeek wrapper
    Co-authored-by: Noah Treuhaft <noah.treuhaft@gmail.com>

--- examples/zeek-wrapper.sh
+++ examples/zeek-wrapper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zeek -C -r - --exec "event zeek_init() { Log::disable_stream(PacketFilter::LOG); Log::disable_stream(LoadedScripts::LOG); }" local
+exec zeek -C -r - --exec "event zeek_init() { Log::disable_stream(PacketFilter::LOG); Log::disable_stream(LoadedScripts::LOG); }" local
